0e3a5e455aca72f18fef39b18ca9a1d1c094b27e
teodoravel <veljanoskateodora9@gmail.com>
2024-11-08 16:21:10 -0500

    Added Issuer and StockPrice models, created database, added IssuerScraperService, updated program.cs, completed filter1

--- StockExchangeAnalysisApp/StockExchangeAnalysisApp/Data/Migrations/20241108173548_MakeIssuerNameNullable.Designer.cs
+++ StockExchangeAnalysisApp/StockExchangeAnalysisApp/Data/Migrations/20241108173548_MakeIssuerNameNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StockExchangeAnalysisApp.Data;
 
@@ -11,9 +12,11 @@ using StockExchangeAnalysisApp.Data;
 namespace StockExchangeAnalysisApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241108173548_MakeIssuerNameNullable")]
+    partial class MakeIssuerNameNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StockExchangeAnalysisApp/StockExchangeAnalysisApp/Models/StockPrice.cs
@@ -0,0 +1,18 @@
+﻿namespace StockExchangeAnalysisApp.Models
+{
+    public class StockPrice
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } // Corresponds to "Date"
+        public decimal LastTradePrice { get; set; } // Corresponds to "Last trade price"
+        public decimal MaxPrice { get; set; } // Corresponds to "Max"
+        public decimal MinPrice { get; set; } // Corresponds to "Min"
+        public decimal AvgPrice { get; set; } // Corresponds to "Avg. Price"
+        public decimal PercentageChange { get; set; } // Corresponds to "%chg."
+        public int Volume { get; set; } // Corresponds to "Volume"
+        public decimal TurnoverBEST { get; set; } // Corresponds to "Turnover in BEST in denars"
+        public decimal TotalTurnover { get; set; } // Corresponds to "Total turnover in denars"
+        public int IssuerId { get; set; } // Foreign key reference to Issuer
+        public Issuer Issuer { get; set; } // Navigation property to Issuer
+    }
+}
